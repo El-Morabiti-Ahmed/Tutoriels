9a72f48f7f9ce3d869be45b26347080cd51c54ab
ahmed <ahmedelmorabiti123460@gmail.com>
2026-09-21 16:25:04 +0000

    rearragened tutoriels

--- realisation/includes/admin_nav.php
+++
@@ -1,7 +0,0 @@
-<?php /* Sub-menu shown on every admin page */ ?>
-<nav class="subnav" aria-label="Admin menu">
-    <a class="<?= Helper::activeClass('admin/index.php') ?>" href="<?= url('admin/index.php') ?>">Dashboard</a>
-    <a class="<?= Helper::activeClass('admin/games.php') ?>" href="<?= url('admin/games.php') ?>">Games</a>
-    <a class="<?= Helper::activeClass('admin/categories.php') ?>" href="<?= url('admin/categories.php') ?>">Categories</a>
-    <a class="<?= Helper::activeClass('admin/players.php') ?>" href="<?= url('admin/players.php') ?>">Players</a>
-</nav>
